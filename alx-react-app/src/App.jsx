84a3f4154840579be7db188c687e3b20cfbfee66
MICHAEL ODHIAMBO <mikeodhis911@gmail.com>
2025-02-22 23:57:07 -0800

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from'./components/Header';
+import Header from './components/Header';
 import MainContent from './components/MainContent';
-import footer from './components/Footer';
-import WelcomeMessage from './components/WelcomeMessage';
+import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-
+import WelcomeMessage from './components/WelcomeMessage';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <Header/>
-        <MainContent/>
-        <WelcomeMessage/>
-        <UserProfile name="alice" age="25" bio="loves hiking and photography"/>
-        <Footer/>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <WelcomeMessage/>
+            <UserProfile name="Alice" age="25" bio="loves hiking and photography"/>
+            <Footer />
+        </div>
+    );
 }
-export default App
+
+export default App;
+
