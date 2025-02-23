804e7295cd1f9cf1482b5d93ebe69a36dfd4b595
MICHAEL ODHIAMBO <mikeodhis911@gmail.com>
2025-02-23 06:04:25 -0800

    update

--- alx-react-app/App.jsx
+++ alx-react-app/App.jsx
@@ -1,8 +1,8 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './UserProfile';
+import WelcomeMessage from './WelcomeMessage';
 function App() {
     return (
         <div>
