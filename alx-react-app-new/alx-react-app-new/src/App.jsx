d259598ed07b0999ff50cd738e792296c156ead7
MICHAEL ODHIAMBO <mikeodhis911@gmail.com>
2025-02-28 22:52:20 -0800

    update

---
+++ alx-react-app-new/alx-react-app-new/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './UserProfile';
+import WelcomeMessage from './WelcomeMessage';
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <WelcomeMessage/>
+            <UserProfile name="Alice" age="25" bio="loves hiking and photography"/>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
