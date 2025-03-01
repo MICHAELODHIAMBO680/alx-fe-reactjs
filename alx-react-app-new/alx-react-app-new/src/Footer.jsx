1caf884e46c6d26855edc62047520e3d2722bd53
MICHAEL ODHIAMBO <mikeodhis911@gmail.com>
2025-02-28 23:00:58 -0800

    update

--- alx-react-app-new/alx-react-app-new/src/Footer.jsx
+++ alx-react-app-new/alx-react-app-new/src/Footer.jsx
@@ -1,9 +1,11 @@
+// Footer.jsx
 function Footer() {
     return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
+      <footer style={{ backgroundColor: 'lightgray', color: 'black', textAlign: 'center', padding: '10px', position: 'fixed', width: '100%', bottom: '0' }}>
+        <p>&copy; 2025 My Company</p>
+      </footer>
     );
-}
-
-export default Footer;
+  }
+  
+  export default Footer;
+  
